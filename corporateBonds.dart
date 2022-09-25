2a8b4f5b5e213f46da79058502511c6111c5f912
Poonyapat Kunlayanopakorn <poonyapat.kun@gmail.com>
2022-09-26 02:20:03 +0700

    Add dd/mm/yy start and end

--- corporateBonds.dart
+++ corporateBonds.dart
@@ -7,27 +7,41 @@ main() {
   int? principle = int.parse(stdin.readLineSync()!);
   print("กรุณาป้อนอัตราดอกเบี้ย(%) : ");
   double? yield = double.parse(stdin.readLineSync()!);
-  print("กรุณาป้อนระยะเวลาหุ้นกู้(วัน) : ");
-  num? duration = num.parse(stdin.readLineSync()!);
-  var now = DateTime.now();
-  var year = now.year;
-  var leapYear = isLeapYear(year);
-  var calendarDay;
-  if (leapYear) {
-    calendarDay = 366;
-  } else {
-    calendarDay = 365;
-  }
-  var interest =
-      ((principle / 100) * (yield / calendarDay) * duration).toStringAsFixed(2);
-  print(
-      "คุณได้รับดอกเบี้ยจำนวน $interest บาท จากหุ้นกู้ $bondName จำนวนเงินลงทุน $principle ด้วยอัตราดอกเบี้ย $yield");
-}
+  print("กรุณาป้อนวันที่เริ่มคิดดอกเบี้ย : (วว/ดด/25ปป) : ");
+  String? start = stdin.readLineSync();
+  print("กรุณาป้อนวันที่สุดท้ายที่คิดดอกเบี้ย : (วว/ดด/25ปป) : ");
+  String? end = stdin.readLineSync();
+  var startDay = start!.substring(0, 2);
+  var startMonth = start.substring(3, 5);
+  var startYear = start.substring(6, 10);
+  int adStartYear = int.parse(startYear) - 543;
+  var endDay = end!.substring(0, 2);
+  var endMonth = end.substring(3, 5);
+  var endYear = end.substring(6, 10);
+  int adEndYear = int.parse(endYear) - 543;
+  print(startDay);
+  print(startMonth);
+  print(adStartYear);
+  print(endDay);
+  print(endMonth);
+  print(adEndYear);
+//   var leapYear = isLeapYear(year);
+//   var calendarDay;
+//   if (leapYear) {
+//     calendarDay = 366;
+//   } else {
+//     calendarDay = 365;
+//   }
+//   var interest =
+//       ((principle / 100) * (yield / calendarDay) * duration).toStringAsFixed(2);
+//   print(
+//       "คุณได้รับดอกเบี้ยจำนวน $interest บาท จากหุ้นกู้ $bondName จำนวนเงินลงทุน $principle ด้วยอัตราดอกเบี้ย $yield");
+// }
 
-bool isLeapYear(int AD) {
-  if (((AD % 4 == 0) && (AD % 100 != 0)) || (AD % 400 == 0)) {
-    return true;
-  } else {
-    return false;
-  }
+// bool isLeapYear(int AD) {
+//   if (((AD % 4 == 0) && (AD % 100 != 0)) || (AD % 400 == 0)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
 }
